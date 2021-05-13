24bacdb0b57c21381cc3ef627aa68fa5671c27b5
vadxq <dongxianlin@vadxq.com>
2021-05-13 15:15:22 +0800

    feat(utils): update get api url

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,4 +1,7 @@
-export const getApiUrl = (url: string): string => {
+/**
+ * @description 获取API URL前缀，根据环境进行判断
+ */
+export const getBaseUrl = (url: string): string => {
   let BASE_URL = '';
   if (process.env.NODE_ENV === 'development') {
     // 开发环境 - 根据请求不同返回不同的BASE_URL
@@ -10,7 +13,7 @@ export const getApiUrl = (url: string): string => {
       BASE_URL = 'http://test-api.vadxq.com';
     }
   } else if (process.env.NODE_ENV === 'mock') {
-    BASE_URL = url;
+    BASE_URL = '';
   } else {
     // 生产环境
     if (url.includes('/api/')) {
@@ -24,4 +27,32 @@ export const getApiUrl = (url: string): string => {
     BASE_URL = BASE_URL.replace('http://', 'https://');
   }
   return BASE_URL;
+};
+
+/**
+ * @description 获取API URL，根据环境进行判断
+ */
+export const getApiUrl = (originUrl: string): string => {
+  const baseUrl: string = getBaseUrl(originUrl);
+  let url: string = originUrl;
+  if (process.env.NODE_ENV === 'development') {
+    // 开发环境 - 根据请求不同返回不同的BASE_URL
+    if (originUrl.includes('/api/')) {
+      url = url.split('/api')[1];
+    }
+    if (originUrl.includes('/web-api/')) {
+      url = url.split('/web-api')[1];
+    }
+  } else if (process.env.NODE_ENV === 'mock') {
+    url = originUrl;
+  } else {
+    // 生产环境
+    if (originUrl.includes('/api/')) {
+      url = url.split('/api')[1];
+    }
+    if (originUrl.includes('/web-api/')) {
+      url = url.split('/web-api')[1];
+    }
+  }
+  return baseUrl + url;
 };
